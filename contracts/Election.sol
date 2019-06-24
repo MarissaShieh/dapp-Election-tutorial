a375198f227964c6870747b57be9e391751603a8
Marissa <marissa.shieh@gmail.com>
2019-06-23 17:12:07 -0700

    Create ways to store each Candidate

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,27 @@
 pragma solidity 0.5.8;
 
 contract Election {
-  // Read/write candidate, a state variable
-  string public candidate;
+  struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
+  }
+
+  //Read/write Candidates
+  mapping (uint => Candidate) public candidates;
+  
+  // Store Candidates Count since Solidity assigns 
+  //default empty values to each key in mapping
+  uint public candidatesCount;
+
+  function addCandidate (string memory _name) private {
+    candidatesCount++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
 
   //constructor 
   constructor () public {
-    candidate = "Candidate 1";
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
   }
 }
